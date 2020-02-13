8981d73651a080f639687c5e335f83fe049ca288
Tudor <tudor.ursu.321@gmail.com>
2020-02-13 09:21:53 +0200

    commit1

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index(){
+    	$hl="Hello, Tudor!";
+    	$td="This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.";
+    	return view('index')->with(['hl'=>$hl,
+    		                        'td'=>$td]);
+    }
+}
