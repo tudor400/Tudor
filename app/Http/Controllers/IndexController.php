9fc4bd32e8c89faa45f4f5f09da055ec262c0829
Tudor <tudor.ursu.321@gmail.com>
2020-02-18 09:27:52 +0200

    composer1

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class IndexController extends Controller
 {
     public function index(){
     	$hl="Hello, Tudor!";
     	$td="This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.";
+    	$articles=Article::all();
     	return view('index')->with(['hl'=>$hl,
-    		                        'td'=>$td]);
+    		                        'td'=>$td,
+    		                        'articles'=>$articles]);
     }
 }
